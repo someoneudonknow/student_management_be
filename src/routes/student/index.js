ff2aa246457f41354564d647befc06640e9c85ca
BangVo123 <vochebang.123@gmail.com>
2024-10-06 21:12:58 +0700

    feat: crud student

--- src/routes/student/index.js
+++ src/routes/student/index.js
@@ -9,5 +9,9 @@ const routes = express.Router();
 
 routes.use(authentication);
 routes.post("/", asyncHandler(StudentController.createStudent));
+routes.get("/:studentId", asyncHandler(StudentController.getStudent));
+routes.get("/", asyncHandler(StudentController.getAllStudents));
+routes.patch("/:studentId", asyncHandler(StudentController.updateStudent));
+routes.delete("/:studentId", asyncHandler(StudentController.deleteStudent));
 
 module.exports = routes;
