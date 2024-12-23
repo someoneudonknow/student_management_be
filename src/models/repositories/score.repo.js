e29bb57d75cd2ef14818f7edec8a98501937a06a
BangVo123 <vochebang.123@gmail.com>
2024-12-23 19:55:23 +0700

    update scores for class

--- src/models/repositories/score.repo.js
+++ src/models/repositories/score.repo.js
@@ -1,35 +1,97 @@
-const DB = require("../../db/mysql.init")
-const {BadRequestError} = require("../../cores/error.response");
+const DB = require("../../db/mysql.init");
+const { BadRequestError } = require("../../cores/error.response");
+const { Op } = require("sequelize");
+const sequelize = require("sequelize");
 
-class ScoreRepository{
-    static async getScores (studentId) {
-        return await DB.Score.findAll({where: {student_id: studentId}})
+class ScoreRepository {
+  static async getScores(studentId) {
+    return await DB.Score.findAll({ where: { student: studentId } });
+  }
+
+  static async getScore(studentId, subjectId) {
+    return await DB.Score.findOne({ where: { subject: subjectId, student: studentId } });
+  }
+
+  static async getScoreOnSubject(subjectId) {
+    return await DB.Score.findAll({ where: { subject: subjectId } });
+  }
+
+  static async getScoreOfClass(classId, subjectId, semester) {
+    return await DB.Score.findAll({
+      include: {
+        model: DB.Student,
+        attribute: ["first_name", "last_name"],
+        where: { class: classId },
+        association: new sequelize.BelongsTo(DB.Score, DB.Student, {
+          targetKey: "id",
+          foreignKey: "student",
+        }),
+      },
+      where: {
+        subject: subjectId,
+        semester,
+        school_year_start: {
+          [Op.eq]: sequelize.literal(`(
+          SELECT MAX(school_year_start) FROM scores
+        )`),
+        },
+      },
+    });
+  }
+
+  static async createScore(payload, options) {
+    await DB.Score.create(payload, options);
+  }
+
+  static async updateScore(studentId, subjectId, semester, payload) {
+    const foundScore = await DB.Score.findOne({
+      where: {
+        subject: subjectId,
+        student: studentId,
+        semester,
+        school_year_start: {
+          [Op.eq]: sequelize.literal(`(
+  SELECT MAX(school_year_start) FROM Scores
+)`),
+        },
+      },
+    });
+
+    const fieldsChange = Object.keys(payload);
+    for (let field of fieldsChange) {
+      foundScore[field] = payload[field];
     }
 
-    static async getScore (studentId, subjectId) {
-        return await DB.Score.findOne({where: {subject_id: subjectId, student_id: studentId}})
-    }
+    return await foundScore.save();
+  }
 
-    static async getScoreOnSubject (subjectId) {
-        return await DB.Score.findAll({where: {subject_id: subjectId}})
-    }
+  static async updateScoresOfClass(subjectId, semester, payloads) {
+    const res = await DB.sequelize.query(
+      "SELECT MAX(school_year_start) as result FROM student_management_dev.scores ORDER BY school_year_start DESC LIMIT 1",
+    );
 
-    static async createScore(payload, options) {
-        await DB.Score.create(payload, options)
-    }
+    const school_year_start = res[0][0]["result"];
 
-    static async updateScore (studentId, subjectId, payload) {
-        const foundScore = await DB.Score.findOne({where: {subject_id: subjectId, student_id: studentId}});
+    const result = await DB.sequelize.transaction(async (t) => {
+      console.log("payloads: ", payloads);
 
-        if(!foundScore) throw new BadRequestError("Score does not exist");
+      for (let payload of payloads) {
+        const { id, ...values } = payload;
 
-        const fieldsChange = Object.keys(payload);
-        for(let field of fieldsChange){
-            foundScore[field] = payload[field];
-        }
+        const query = await DB.Score.update(values, {
+          where: {
+            student: id,
+            subject: subjectId,
+            semester,
+            school_year_start,
+          },
+          transaction: t,
+        });
+      }
+    });
 
-        return await foundScore.save();
-    }
+    return result;
+  }
 }
 
-module.exports = ScoreRepository
+module.exports = ScoreRepository;
